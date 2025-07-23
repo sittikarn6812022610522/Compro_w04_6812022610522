d1a1187e9b33f3a4637899dcb111f22cd3c2de93
Sittikarn <s6812022610522@email.kmutnb.ac.th>
2025-07-23 12:04:27 +0700

    coding_w04-02

---
+++ Coding_w04-02.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // studio.h --> <stdio.h>
+
+int main ()  {
+    int a = 10; // int a = 10 --> int a = 10; ประเภทข้อมูล int คัวเเปร a ค่าที่เก็บ 10
+    printf(" a = %d", a); // printf(" a = %d", a) --> printf(" a = %d", a);
+    return 0 ; //  return 0  -->  return 0 ;
+
+}
